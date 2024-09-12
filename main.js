6111f5efe775cf757faa00d50bbe3beb462c8f55
AshwagAlharthi <Ashwag.alharthi@hotmail.com>
2024-09-12 13:28:48 +0300

    solved

---
+++ main.js
@@ -0,0 +1,49 @@
+let one = document.getElementById("one");
+let two = document.getElementById("two");
+let three = document.getElementById("three");
+let four = document.getElementById("four");
+let five = document.getElementById("five");
+let six = document.getElementById("six");
+let seven = document.getElementById("seven");
+let eight = document.getElementById("eight");
+let nine = document.getElementById("nine");
+
+let screen = document.getElementById("screen");
+
+one.addEventListener("click", () => {
+    imgConatin.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/blue-planet.jpg";
+})
+
+two.addEventListener("click", () => {
+    imgConatin.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/deadpool.jpg";
+
+})
+
+three.addEventListener("click", () => {
+    imgConatin.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/gravity.jpg";
+})
+
+four.addEventListener("click", () => {
+    imgConatin.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/good-will-hunting.jpg";
+})
+
+five.addEventListener("click", () => {
+    imgConatin.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/cnn.jpg";
+})
+
+six.addEventListener("click", () => {
+    imgConatin.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/koyaanisqatsi.jpg";
+})
+
+seven.addEventListener("click", () => {
+    imgConatin.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/parts-unknown.jpg";
+})
+
+eight.addEventListener("click", () => {
+    imgConatin.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/star-wars-solo.jpeg";
+})
+
+nine.addEventListener("click", () => {
+    imgConatin.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/stranger-things.jpg";
+})
+
